cc0f9a18352d1b981cd4c611667f194ab7699a7a
Prathu Singh <prathu@tvisioninsights.com>
2019-02-18 09:08:41 -0500

    Configured mongoose Updated babel configuration

--- server/src/index.js
+++ server/src/index.js
@@ -1,11 +1,21 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import db from './config/keys.js';
 
 const app = express();
 
-//Body parser middleware
+// Body parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json);
+
+// Connect to db
+mongoose
+.connect(db.mongoURI , { useNewUrlParser: true })
+.then(() => { console.log('DB connected')} )
+.catch((err) => {console.log(err)});
+
+
 
 const port = process.env.PORT || 8000;
 
