50dfc39fa698bbb9bc4b6aa3e428a740813c168e
Prathu Singh <prathu@tvisioninsights.com>
2019-02-19 06:39:53 -0500

    Register user with hashed password

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -1,9 +1,9 @@
 import mongoose from "mongoose";
-import bcrypt from "bcrypt";
 
 const UserSchema = new mongoose.Schema({
+  name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true }
 });
 
-export default UserSchema;
+export default mongoose.model("User", UserSchema);
