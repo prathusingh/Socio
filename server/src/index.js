af3185fe0add557ddea40656a9168ca03d300cf2
Prathu Singh <prathu@tvisioninsights.com>
2019-02-18 22:05:26 -0500

    Initialized user model and user route

--- server/src/index.js
+++ server/src/index.js
@@ -1,22 +1,27 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
-import db from './config/keys.js';
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import db from "./config/keys.js";
+import users from "./routes/api/users";
 
 const app = express();
 
 // Body parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json);
 
 // Connect to db
 mongoose
-.connect(db.mongoURI , { useNewUrlParser: true })
-.then(() => { console.log('DB connected')} )
-.catch((err) => {console.log(err)});
+  .connect(db.mongoURI, { useNewUrlParser: true })
+  .then(() => {
+    console.log("DB connected");
+  })
+  .catch(err => {
+    console.log(err);
+  });
 
-
+// Use routes
+app.use("/api/users", users);
 
 const port = process.env.PORT || 8000;
 
-app.listen(port, () =>  console.log(`Server running on ${port}`));
+app.listen(port, () => console.log(`Server running on ${port}`));
