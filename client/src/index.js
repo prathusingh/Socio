6b18d5a07d149a7fc39fd7efd8f8e61b25080560
Prathu Singh <prathu@tvisioninsights.com>
2019-03-13 21:01:20 -0400

    Added formik to login form

--- client/src/index.js
+++ client/src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './index.css';
 import App from './App';
-import { Login } from './components/login/Login';
+import { LoginForm } from './components/login/Login';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <Router>
     <div>
       <Route exact path="/" component={App} />
-      <Route exact path="/login" component={Login} />
+      <Route exact path="/login" component={LoginForm} />
     </div>
   </Router>,
   document.getElementById('root')
