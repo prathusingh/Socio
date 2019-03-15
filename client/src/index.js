08ad129a73a2de782d86797ce0ebf4a70f2003a7
Prathu Singh <prathu@tvisioninsights.com>
2019-03-14 21:33:36 -0400

    configured-reducer

--- client/src/index.js
+++ client/src/index.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import './index.css';
+import { Provider } from 'react-redux';
+
+import './index.scss';
 import App from './App';
 import { LoginForm } from './components/login/Login';
+import store from './store';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <Router>
-    <div>
-      <Route exact path="/" component={App} />
-      <Route exact path="/login" component={LoginForm} />
-    </div>
-  </Router>,
+  <Provider store={store}>
+    <Router>
+      <div>
+        <Route exact path="/" component={App} />
+        <Route exact path="/login" component={LoginForm} />
+      </div>
+    </Router>
+  </Provider>,
   document.getElementById('root')
 );
 
