8add7f988cb1dd90d77472da28b38f8f408e1675
Prathu Singh <prathu@tvisioninsights.com>
2019-03-23 14:59:23 -0400

    added login action

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,10 +1,24 @@
 import axios from 'axios';
 import { GET_ERRORS } from './types';
 
+// register user
 export const registerUser = (userData, history) => dispatch => {
   axios
     .post('http://localhost:8000/api/users/register', userData)
     .then(res => history.push('./login'))
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+// login user
+export const loginUser = userData => dispatch => {
+  axios
+    .post('http://localhost:8000/api/users/login', userData)
+    .then(res => console.log(res))
     .catch(err =>
       dispatch({
         type: GET_ERRORS,
