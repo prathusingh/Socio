fa1e5bfaac58c43f41d3718dd439be3f0faccd0f
Prathu Singh <prathusingh21@gmail.com>
2019-04-14 18:02:53 -0400

    Added support for async and generator by specifying entry point in config

--- client/src/config/webpack.base.config.js
+++ client/src/config/webpack.base.config.js
@@ -5,10 +5,13 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
+const APP_DIR = path.resolve(__dirname, '../src');
+
 module.exports = env => {
   const { PLATFORM, VERSION } = env;
   return merge([
     {
+      entry: ['@babel/polyfill', APP_DIR],
       module: {
         rules: [
           {
