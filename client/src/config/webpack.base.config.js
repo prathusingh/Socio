83b014b4048dc0cec73803e84c8d7b605d416334
Prathu Singh <prathusingh21@gmail.com>
2019-04-14 16:38:39 -0400

    Added webpack to client

---
+++ client/src/config/webpack.base.config.js
@@ -0,0 +1,37 @@
+const webpack = require('webpack');
+const merge = require('webpack-merge');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = env => {
+  const { PLATFORM, VERSION } = env;
+  return merge([
+    {
+      module: {
+        rules: [
+          {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+              loader: 'babel-loader'
+            }
+          },
+          {
+            test: /\.scss$/,
+            use: ['style-loader', 'css-loader', 'sass-loader']
+          }
+        ]
+      },
+      plugins: [
+        new HtmlWebpackPlugin({
+          template: './public/index.html',
+          filename: './index.html'
+        }),
+        new webpack.DefinePlugin({
+          'process.env.VERSION': JSON.stringify(env.VERSION),
+          'process.env.PLATFORM': JSON.stringify(env.PLATFORM)
+        })
+      ]
+    }
+  ]);
+};
