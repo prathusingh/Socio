fd9a9915cc5e084a0fb737665c5050143f237533
Prathu Singh <prathusingh21@gmail.com>
2019-05-03 18:18:11 -0400

    Built inverted index

--- engine/search/build_index.py
+++ engine/search/build_index.py
@@ -7,7 +7,7 @@ class BuildIndex:
     def __init__(self, files):
         self.filenames = files
         self.file_to_tokens = self.process_files()
-        self.make_indices(self.file_to_tokens)
+        self.build_inverted_index(self.file_to_tokens)
 
     def process_files(self):
         file_to_tokens = {}
@@ -33,9 +33,31 @@ class BuildIndex:
                 file_index[token] = [index]
         return file_index
 
+    # input = {filename: [word1, word2...]}
+    # output = {filename: {word1: [pos1,pos2]}, ....}
     def make_indices(self, file_to_tokens):
+        document_mapper = {}
         for file in file_to_tokens:
-            print (BuildIndex.index_one_file(self.file_to_tokens[file]))
+            document_mapper[file] = BuildIndex.index_one_file(self.file_to_tokens[file])
+        return document_mapper
 
+    # input = {filename: {word: [pos1, pos2]}, word2: [pos5, pos8]}, filename2:.....}
+    # output = {word: {filename:[pos1, pos2..]..}}
+    def build_index(self, doc_map):
+        inverted_index = {}
 
+        for filename in doc_map:
+            words_pos_map = doc_map[filename]
+            for word in words_pos_map:
+                if word in inverted_index.keys():
+                    existing_map = inverted_index[word]
+                    existing_map[filename] = words_pos_map[word]
+                    inverted_index[word] = existing_map
+                else:
+                    inverted_index[word] = {filename: words_pos_map[word]}
+
+        return inverted_index
+
+    def build_inverted_index(self, file_tokens):
+        self.build_index(self.make_indices(file_tokens))
 
