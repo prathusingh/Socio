32d413bf1cb72c25bd252a7ee3302a257ea5b2e0
Prathu Singh <prathusingh21@gmail.com>
2019-05-03 08:10:32 -0400

    Calculate postion of each word in a document so that phrase query can be answered

--- engine/search/build_index.py
+++ engine/search/build_index.py
@@ -7,8 +7,7 @@ class BuildIndex:
     def __init__(self, files):
         self.filenames = files
         self.file_to_tokens = self.process_files()
-        for key in self.file_to_tokens:
-            print(self.file_to_tokens[key])
+        self.make_indices(self.file_to_tokens)
 
     def process_files(self):
         file_to_tokens = {}
@@ -21,3 +20,22 @@ class BuildIndex:
             file_to_tokens[file] = [word for word in file_to_tokens[file] if word not in stopwords.words('english')]
 
         return file_to_tokens
+
+    # input = [word1, word2 .....]
+    # output = {word1: [pos1, pos2], word2: [pos6, pos46]......}
+    @staticmethod
+    def index_one_file(tokens):
+        file_index = {}
+        for index, token in enumerate(tokens):
+            if token in file_index.keys():
+                file_index[token].append(index)
+            else:
+                file_index[token] = [index]
+        return file_index
+
+    def make_indices(self, file_to_tokens):
+        for file in file_to_tokens:
+            print (BuildIndex.index_one_file(self.file_to_tokens[file]))
+
+
+
