c8ebac65eabd13c103ca36c06ba81c4c42cf0876
Prathu Singh <prathusingh21@gmail.com>
2019-05-25 16:57:28 -0400

    Removed unwanted code

--- client/src/config/keys.js
+++
@@ -1,9 +0,0 @@
-import dotenv from 'dotenv';
-
-//configure parsing for env file
-dotenv.config();
-
-export default {
-  googleClientId: process.env.GOOGLE_CLIENT_ID,
-  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET
-};
