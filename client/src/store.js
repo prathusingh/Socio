d65fc981d5e496e10777b03f2d613b88dab209a5
Prathu Singh <prathusingh21@gmail.com>
2019-06-23 22:36:52 -0400

    Fixed redux dev tools extensionw hich broje after webpack upgrade

--- client/src/store.js
+++ client/src/store.js
@@ -1,17 +1,16 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 const initialState = {};
 
 const middleware = [thunk];
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION__COMPOSE || compose;
-
 const store = createStore(
   rootReducer,
   initialState,
-  composeEnhancers(applyMiddleware(...middleware))
+  composeWithDevTools(applyMiddleware(...middleware))
 );
 
 export default store;
