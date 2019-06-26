c2d922cdeed80145f617a83749a72991732c2a8e
Prathu Singh <prathusingh21@gmail.com>
2019-06-26 18:49:08 -0400

    Session timeout if there is no user activity

--- client/src/components/session-manager/SessionManager.js
+++ client/src/components/session-manager/SessionManager.js
@@ -1,30 +1,21 @@
 import React from 'react';
+import IdleTimer from 'react-idle-timer';
+
 import { SessionTimeoutModal } from '../modals/session-timeout/SessionTimeoutModal';
 import { logoutUser } from '../../actions/authActions';
+import constants from '../../utils/constants';
 
 export class SessionManager extends React.Component {
   constructor(props) {
     super(props);
+    this.idleTimer = null;
+    this.onAction = this._onAction.bind(this);
+    this.onActive = this._onActive.bind(this);
+    this.onIdle = this._onIdle.bind(this);
     this.state = {
       show: false,
-      inactivityThreshold: 3600000, // 1 hour
       timer: undefined
     };
-  }
-
-  startTimer() {
-    if (typeof this.state.timer !== 'undefined') {
-      clearTimeout(this.state.timer);
-    }
-    const newTimer = setTimeout(() => {
-      this.showModal();
-    }, this.state.inactivityThreshold);
-
-    this.setState({ timer: newTimer });
-  }
-
-  componentWillMount() {
-    this.startTimer();
   }
 
   showModal = () => {
@@ -41,9 +32,35 @@ export class SessionManager extends React.Component {
     this.props.history.push('./login');
   };
 
+  _onAction(e) {
+    console.log('user did something', e);
+  }
+
+  _onActive(e) {
+    console.log('user is active', e);
+    console.log('time remaining', this.idleTimer.getRemainingTime());
+  }
+
+  _onIdle(e) {
+    console.log('user is idle', e);
+    console.log('last active', this.idleTimer.getLastActiveTime());
+    this.showModal();
+  }
+
   render() {
     return (
       <div>
+        <IdleTimer
+          ref={ref => {
+            this.idleTimer = ref;
+          }}
+          element={document}
+          onActive={this.onActive}
+          onIdle={this.onIdle}
+          onAction={this.onAction}
+          debounce={250}
+          timeout={constants.userInactivityThreshold}
+        />
         <button type="button" onClick={this.handleLogout}>
           Logout
         </button>
@@ -51,7 +68,6 @@ export class SessionManager extends React.Component {
           show={this.state.show}
           handleContinue={this.hideModal}
           handleLogout={this.handleLogout}
-          inactivityThreshold={this.inactivityThreshold}
         >
           <p>You are being timedoff due to inactivity.</p>
           <p>Please choose to stay signed in or to logoff.</p>
---
+++ client/src/utils/constants.js
@@ -0,0 +1,3 @@
+export default {
+  userInactivityThreshold: 3600000 // 1 hour
+};
