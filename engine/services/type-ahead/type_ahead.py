620775d033f20133444e91d5a91d03af07d01fed
Prathu Singh <prathusingh21@gmail.com>
2019-06-27 19:43:56 -0400

    my commit fillers when I don't know how to implement

--- engine/services/type-ahead/type_ahead.py
+++ engine/services/type-ahead/type_ahead.py
@@ -11,8 +11,16 @@ class TypeAhead:
     def get_auto_complete_results(self, prefix):
         self.storage.prefix_storage.fetch_items_for_given_prefix(prefix)
 
+    def sort_auto_complete_results(self, results_list):
+        # TODO: Implement it
+        pass
+
+    def get_topmost_5_completions(self, prefix):
+        auto_complete_results = self.get_auto_complete_results(prefix)
+        return self.sort_auto_complete_results(auto_complete_results)
+
 
 # data
 data = ['cricket', 'diving', 'cooking', 'tutoring', 'skating']
 type_ahead = TypeAhead(data)
-type_ahead.get_auto_complete_results('cri')
+type_ahead.get_topmost_5_completions('cri')
