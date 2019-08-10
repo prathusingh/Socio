d0fed6a0af7a59995909534dbd6b63839244d05b
Prathu Singh <prathusingh21@gmail.com>
2019-08-10 14:10:36 -0400

    react font awesome icons not working deferred to cdn

--- client/src/components/spinner/Spinner.js
+++ client/src/components/spinner/Spinner.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSpinner } from '@fortawesome/free-solid-svg-icons';
+import { faSpinner, faPuls } from '@fortawesome/free-solid-svg-icons';
 import './Spinner.scss';
 
 export function Spinner() {
   return (
     <span className="spinner">
-      <FontAwesomeIcon icon={faSpinner} />
+      <i className="fa fa-spinner fa-spin" />
     </span>
   );
 }
